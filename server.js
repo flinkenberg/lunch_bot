fee65fac186bfdb1f219aa9706c4d3ae427afe98
Ondrej Stastny <ondrej@ajenta.net>
2018-10-26 20:29:42 +0100

    Test

--- server.js
+++ server.js
@@ -1,13 +1,18 @@
+require("dotenv").config();
 const fetch = require("node-fetch");
 const express = require("express");
 const bodyParser = require("body-parser");
-const app = express().use(bodyParser.urlencoded({ extended: false }));
-const port = 6000;
+const app = express();
+// const app = express();
+const port = process.env.PORT;
 
-app.get("/", (req, res) => {
-    console.log(req.headers);
-    return res.send("tets");
-});
+// app.get("/", (req, res) => {
+//     console.log(req.headers);
+//     return res.send("tets");
+// });
+
+let step = 1;
+let lastMessage = null;
 
 const fields = [
     "firstname",
@@ -62,7 +67,7 @@ async function orderLunch(params) {
     }
 }
 
-app.post("/", async (req, res, next) => {
+app.use(bodyParser.urlencoded({ extended: false })).post("/", async (req, res, next) => {
     try {
         let params = req.body.text.split(" ");
         params = params.reduce((acc, p) => {
@@ -76,18 +81,14 @@ app.post("/", async (req, res, next) => {
                             if (!wraps.includes(value)) {
                                 console.log(params);
                                 throw new Error(`Not a valid wrap. Use one of the following wraps: [${wraps}].`);
-                            } else {
-                                acc = {
-                                    ...acc,
-                                    [key]: value,
-                                };
+                            } else if (key === "firstname" || key === "lastname") {
+                                value = value.toUpperCase();
                             }
-                        } else {
-                            acc = {
-                                ...acc,
-                                [key]: value,
-                            };
                         }
+                        acc = {
+                            ...acc,
+                            [key]: value,
+                        };
                     } else {
                         console.log(params);
                         throw new Error(`Invalid fields. Use one of the following fields: [${fields}]`);
@@ -103,10 +104,10 @@ app.post("/", async (req, res, next) => {
                 res.json({
                     "response_type": "in_channel",
                     "text": `
-                    > :harold: *Hello, here is your order*:
-                        :yvan_no: *Wrap:* \`\`\`${wrapsEnum[params.wrap]}\`\`\`
-                        :i_see: *Name:* \`\`\`${params.firstname} ${params.lastname}\`\`\` 
-                        :ondrej: *Phone:* \`\`\`${params.phone}\`\`\`
+                    > :happy_dev: *Hello, here is your order*: :harold:
+                        :yvan: *Wrap:* \`\`\`${wrapsEnum[params.wrap]}\`\`\`
+                        :i_see: *Name:* \`\`\`${params.firstname.toUpperCase()} ${params.lastname.toUpperCase()}\`\`\`
+                        :not_sure: *Phone:* \`\`\`${params.phone}\`\`\`
                     `,
                 });
             }
@@ -117,6 +118,66 @@ app.post("/", async (req, res, next) => {
     } catch(err) {
         res.send(err.toString());
         next(err);
+    }
+});
+
+app.use(bodyParser.json()).post("/events", async (req, res) => {
+    // res.json({
+    //     challenge: req.body.challenge,
+    // });
+    console.log(req.body);
+    if (req.body.event.text) {
+        if (req.body.event.type === "app_mention") {
+            res.sendStatus(200);
+            step = 1;
+            const reply = await fetch("https://slack.com/api/chat.postMessage", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                    "Authorization": `Bearer ${process.env.BOT_TOKEN}`,
+                },
+                body: JSON.stringify({
+                    text: `Hi, which wrap would you like to order <@${req.body.event.user}>? :happy_dev:`,
+                    channel: req.body.event.channel,
+                }),
+            });
+            if (reply.status === 200) {
+                step++;
+            }
+            // const redsadsa = await reply.json();
+            // console.log(redsadsa)
+        } else if (req.body.event.type === "message" && req.body.event.user) {
+            res.sendStatus(200);
+            if (step === 2 && wraps.includes(req.body.event.text)) {
+                const reply = await fetch("https://slack.com/api/chat.postMessage", {
+                    method: "POST",
+                    headers: {
+                        "Content-Type": "application/json",
+                        "Authorization": `Bearer ${process.env.BOT_TOKEN}`,
+                    },
+                    body: JSON.stringify({
+                        text: `Mmm, ${wrapsEnum[req.body.event.text]}, okay. What is your full name? :harold:`,
+                        channel: req.body.event.channel,
+                    }),
+                });
+                if (reply.status === 200) {
+                    step++;
+                }
+            } else if (req.body.event.user) {
+                res.sendStatus(200);
+                const reply = await fetch("https://slack.com/api/chat.postMessage", {
+                    method: "POST",
+                    headers: {
+                        "Content-Type": "application/json",
+                        "Authorization": `Bearer ${process.env.BOT_TOKEN}`,
+                    },
+                    body: JSON.stringify({
+                        text: `Sorry I did not get that... :i_see:`,
+                        channel: req.body.event.channel,
+                    }),
+                });
+            }
+        }
     }
 });
 
